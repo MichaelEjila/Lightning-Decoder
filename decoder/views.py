54ccc7793708d13e5a8284212529d35a50438904
Michael Ejila <mac@Michaels-MBP.lan>
2022-06-22 17:33:35 +0100

    Added lninvoice view and fields for template

--- decoder/views.py
+++ decoder/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from decouple import config
+import json
 
 # Create your views here.
 def index(request):
+    
     return render(request, 'decoder/base.html', {})
 
 
@@ -11,10 +15,38 @@ def lnurl(request):
     return render(request, 'decoder/lnurl.html', {})
 
 def lninvoice(request):
-
-    return render(request, 'decoder/lninvoice.html', {})
+    parsedData = []
+    if request.method == 'POST':
+        invoice = request.POST.get('lninvoice')
+        
+        api_key = config('key')
+        url = "https://sandboxapi.bitnob.co/api/v1/wallets/ln/decodepaymentrequest"
     
+        payload = {"request": invoice}
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_key
+        }
+        
+        response = requests.post(url, json=payload, headers=headers)
+        jsonList = []
+        jsonList.append(json.loads(response.content))
+        userData = {}
+        for data in jsonList:
+            userData['status'] = data['status']
+            userData['message'] = data['message']
+            userData['chain address'] = data['data']['chain_address']
+            userData['Description'] = data['data']['description']
+            userData['expires_at'] = data['data']['expires_at']
+            userData['is_expired'] = data['is_expired']
+            
+        parsedData.append(userData)
+
+
+    return render(request, 'decoder/lninvoice.html', {'response':parsedData})
+
 def lnaddress(request):
 
-    return render(request, 'decoder/lnadress.html', {})
+    return render(request, 'decoder/lnaddress.html', {})
     
--- lightning_decoder/settings.py
+++ lightning_decoder/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'lightning_decoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

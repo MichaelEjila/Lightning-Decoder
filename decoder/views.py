87ff26d50c1d69f9468b2d2fb1b3856d5782718e
Michael Adebambo <80793630+Blazing-Mike@users.noreply.github.com>
2022-06-22 17:21:07 +0100

    wrote vies for lnaddress

--- decoder/views.py
+++ decoder/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+import json
+from decouple import config
 
 # Create your views here.
 def index(request):
@@ -15,6 +18,32 @@ def lninvoice(request):
     return render(request, 'decoder/lninvoice.html', {})
     
 def lnaddress(request):
+    API_KEY = config('KEY')
+    url = "https://sandboxapi.bitnob.co/api/v1/lnurl/decodelnaddress"
 
-    return render(request, 'decoder/lnadress.html', {})
+    payload = {"lnAddress": "mikeoxygen@bitnob.io"}
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": "Bearer "+ API_KEY
+}
+    jsonList = []
+    req = requests.post(url, json=payload, headers=headers)
+    content = req.text
+    jsonList.append(json.loads(req.content))
+    parsedData = []
+    userData = {}
+    for data in jsonList:
+     userData['status'] = data['status']
+     userData['message'] = data['message']
+     userData['data-tag'] = data['data']['tag']
+     userData['data-metadata'] = data['data']['metadata']
+     
+
+
+
+    parsedData.append(userData)
+    return HttpResponse(parsedData)
+    print(req.text)
+
     

3612351998f22309cc58544722c2fd9d6cb2c5fa
Michael Ejila <mac@Michaels-MBP.lan>
2022-06-22 10:04:32 +0100

    Added templates

--- decoder/views.py
+++ decoder/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Base Page")
+    return render(request, 'decoder/base.html', {})
 
 
 def lnurl(request):
-    return HttpResponse("Lightning Url page")
+
+    return render(request, 'decoder/lnurl.html', {})
 
 def lninvoice(request):
-    return HttpResponse("Lightning Invoice page")
 
+    return render(request, 'decoder/lninvoice.html', {})
 def lnaddress(request):
-    return HttpResponse("Lightning Address page")
+
+    return render(request, 'decoder/lnadress.html', {})
     

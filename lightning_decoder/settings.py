0c7e05edb301595a7e7766932f81b920d4763ad9
Michael Ejila <mac@Michaels-MBP.lan>
2022-06-23 20:18:28 +0100

    Added heroku to allowed_host

--- lightning_decoder/settings.py
+++ lightning_decoder/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['lightning-decoder.herokuapp.com']
 
 
 # Application definition

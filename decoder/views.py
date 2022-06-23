babb55ece82f14bd6225653772b64f81f2a1721f
Michael Ejila <mac@Michaels-MBP.lan>
2022-06-23 11:27:46 +0100

    linked lnurl view to template

--- decoder/views.py
+++ decoder/views.py
@@ -12,31 +12,35 @@ def index(request):
   
 #Extract data in an organized form
 def lnurl(request):
+    parsedData = ''
+    if request.method == 'POST':
+        lightning_url = request.POST.get('lnurl')
 
-    api_key = config('key')
-    url = "https://sandboxapi.bitnob.co/api/v1/lnurl/decodelnurl"
-    payload = {"encodedLnUrl": "lnurl1dp68gurn8ghj7cnfw3ex2enfd3kzumt99amrztmvde6hymzlwpshjtekxycnvef48yun2wp4xs6kgvpsx93kgdp5ve3nvk9dgjd"}
-    headers = {
-        "Accept": "application/json",
-        "Content-Type" : "application/json",
-        "Authorization" : "Bearer " + api_key
-    }
-    response = requests.post(url, json=payload, headers=headers)
+        api_key = config('key')
+        url = "https://sandboxapi.bitnob.co/api/v1/lnurl/decodelnurl"
+        payload = {"encodedLnUrl": lightning_url}
+        headers = {
+            "Accept": "application/json",
+            "Content-Type" : "application/json",
+            "Authorization" : "Bearer " + api_key
+        }
+        response = requests.post(url, json=payload, headers=headers)
 
-    jsonList = []
-    jsonList.append(json.loads(response.content))
-    parsedData = []
-    lnData = {}
-    for data in jsonList:
-        lnData['status'] = data['status']
-        lnData['message'] = data['message']
-        lnData['tag'] = data['data']['tag']
-        lnData['callback'] = data['data']['callback']
-        lnData['description'] = data['data']['description']
-        lnData['satMinSendable'] = data['data']['satMinSendable']
-        lnData['satMaxSendable'] = data['data']['satMaxSendable']
-    parsedData.append(lnData)
-    return HttpResponse(parsedData)
+        jsonList = []
+        jsonList.append(json.loads(response.content))
+        parsedData = []
+        lnData = {}
+        for data in jsonList:
+            lnData['status'] = data['status']
+            lnData['message'] = data['message']
+            lnData['tag'] = data['data']['tag']
+            lnData['callback'] = data['data']['callback']
+            lnData['description'] = data['data']['description']
+            lnData['satMinSendable'] = data['data']['satMinSendable']
+            lnData['satMaxSendable'] = data['data']['satMaxSendable']
+        parsedData.append(lnData)
+
+    return render(request, 'decoder/lnurl.html', {'response':parsedData})
 
 
 def lninvoice(request):
